2a80c8d01943622cf696740596469649c4a1c2fa
Maximilian <73437063+WildCooding@users.noreply.github.com>
2023-06-01 10:15:00 +0200

    Update PHP, PHPMyadmin und MySQL Installieren und Konfigurieren.sh

--- PHP, PHPMyadmin und MySQL Installieren und Konfigurieren.sh
+++ PHP, PHPMyadmin und MySQL Installieren und Konfigurieren.sh
@@ -1,33 +1,47 @@
 #!/bin/bash
 
-# MySQL Installation
-echo "MySQL Installation"
-read -p "MySQL Root-Passwort [root]: " mysql_password
-mysql_password=${mysql_password:-root}  # Standardwert 'root' verwenden
+#COLORS
+# Reset
+Color_Off='\033[0m'       # Text Reset
 
-# Apache Installation
-echo "Apache Installation"
-# Füge hier weitere Apache-Konfigurationen hinzu, falls erforderlich
+# Regular Colors
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
 
-# PHPMyAdmin Installation
-echo "phpMyAdmin Installation"
-read -p "PHPMyAdmin Passwort [root]: " phpmyadmin_password
-phpmyadmin_password=${phpmyadmin_password:-root}  # Standardwert 'root' verwenden
+# Update packages and Upgrade system
+echo -e "$Cyan \n Updating System.. $Color_Off"
+sudo apt-get update -y && sudo apt-get upgrade -y
 
-# Pakete installieren
-echo "Pakete installieren"
-sudo apt-get update
-sudo apt-get install -y apache2 mysql-server phpmyadmin
+## Install AMP
+echo -e "$Cyan \n Installing Apache2 $Color_Off"
+sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
 
-# MySQL konfigurieren
-echo "MySQL konfigurieren"
-sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$mysql_password'; FLUSH PRIVILEGES;"
+echo -e "$Cyan \n Installing PHP & Requirements $Color_Off"
+sudo apt-get install libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-mcrypt php5-mysql php5-ps php5-pspell php5-recode php5-xsl -y
 
-# Apache konfigurieren
-echo "Apache konfigurieren"
-sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
-sudo a2enconf phpmyadmin.conf
-sudo systemctl restart apache2
+echo -e "$Cyan \n Installing MySQL $Color_Off"
+sudo apt-get install mysql-server mysql-client libmysqlclient15.dev -y
 
-# Abschluss
-echo "Installation abgeschlossen!"
+echo -e "$Cyan \n Installing phpMyAdmin $Color_Off"
+sudo apt-get install phpmyadmin -y
+
+echo -e "$Cyan \n Verifying installs$Color_Off"
+sudo apt-get install apache2 libapache2-mod-php5 php5 mysql-server php-pear php5-mysql mysql-client mysql-server php5-mysql php5-gd -y
+
+## TWEAKS and Settings
+# Permissions
+echo -e "$Cyan \n Permissions for /var/www $Color_Off"
+sudo chown -R www-data:www-data /var/www
+echo -e "$Green \n Permissions have been set $Color_Off"
+
+# Enabling Mod Rewrite, required for WordPress permalinks and .htaccess files
+echo -e "$Cyan \n Enabling Modules $Color_Off"
+sudo a2enmod rewrite
+sudo php5enmod mcrypt
+
+# Restart Apache
+echo -e "$Cyan \n Restarting Apache $Color_Off"
+sudo service apache2 restart
